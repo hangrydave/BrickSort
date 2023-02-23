ad350d86421c08313e318e688e6483dcc9829fb7
David Reidsma <david.reidsma@gmail.com>
2023-02-23 14:15:14 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BrickSort',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
